c6fcb008a716d035dd11c62f76e3d342318c01e8
juanvleal <juanvleal@gmail.com>
2023-01-16 09:41:19 -0300

    initial commit

---
+++ src/main/java/com/toolsrepository/ToolsrepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.toolsrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsrepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolsrepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toolsrepository/ToolsrepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toolsrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsrepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
