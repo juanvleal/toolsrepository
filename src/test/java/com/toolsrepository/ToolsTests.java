7d8f7771abebf31f4b263b1ecc17c1fbc34d3d9a
juanvleal <juanvleal@gmail.com>
2023-01-17 11:17:38 -0300

    test: Fix tests bugs.

--- src/test/java/com/toolsrepository/ToolsTests.java
+++ src/test/java/com/toolsrepository/ToolsTests.java
@@ -9,8 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class ToolsTests {
@@ -18,13 +17,22 @@ public class ToolsTests {
     @Autowired
     ToolRepository toolRepository;
 
+    Tool createTool(){
+        Tool tool = new Tool();
+        tool.setTitle("New Tool to Test");
+        tool.setDescription("JpaRepository is particularly a JPA specific extension for Repository.");
+        tool.setLink("https://docs.spring.io/spring-data/jpa/docs/current/api/org/springframework/data/jpa/repository/JpaRepository.html");
+        return toolRepository.save(tool);
+    }
+
     @Test
     void createToolTest(){
         Tool tool = new Tool();
-        tool.setTitle("JpaRepository");
+        tool.setTitle("Testando nova tool");
         tool.setDescription("JpaRepository is particularly a JPA specific extension for Repository.");
         tool.setLink("https://docs.spring.io/spring-data/jpa/docs/current/api/org/springframework/data/jpa/repository/JpaRepository.html");
         toolRepository.save(tool);
+        toolRepository.deleteById(tool.getId());
     }
 
     @Test
@@ -33,22 +41,28 @@ public class ToolsTests {
         assertThat(tools).isNotNull();
     }
     @Test
-    void listToolByIdTest(){
-        Tool tool = toolRepository.findById(13L).get();
-        assertEquals("JpaRepository Documentation", tool.getTitle());
+    void listToolByIdTest() {
+        Tool tool =  createTool();
+        toolRepository.findById(tool.getId()).get();
+        assertEquals("https://docs.spring.io/spring-data/jpa/docs/current/api/org/springframework/data/jpa/repository/JpaRepository.html", tool.getLink());
+        toolRepository.deleteById(tool.getId());
     }
 
     @Test
     void updateToolTest(){
-        Tool tool = toolRepository.findById(13L).get();
-        tool.setTitle("JpaRepository Documentation");
+        Tool tool =  createTool();
+        Tool toolUp = toolRepository.findById(tool.getId()).get();
+        toolUp.setTitle("Nova Tool testada.");
         toolRepository.save(tool);
-        assertNotEquals("JpaRepository", tool.getTitle());
+        assertNotEquals("Testando nova tool", tool.getTitle());
+        toolRepository.deleteById(tool.getId());
     }
 
     @Test
     void deleteToolTest(){
-        toolRepository.deleteById(10L);
-        assertThat(toolRepository.existsById(10L));
+        Tool tool =  createTool();
+        Long toolId = tool.getId();
+        toolRepository.deleteById(toolId);
+        assertFalse(toolRepository.existsById(toolId));
     }
 }
